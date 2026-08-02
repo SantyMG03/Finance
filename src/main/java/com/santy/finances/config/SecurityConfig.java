15aa6d9c48caa41ae37931d2d0a70c4a1d08b53c
SantyMG03 <santymorenog2003@gmail.com>
2026-08-02 19:33:30 +0200

    Added basic security config

---
+++ src/main/java/com/santy/finances/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.santy.finances.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // This is just for testing myself
+                // TODO: Implement JWToken for more advance user administration
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
