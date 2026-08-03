0856da860a000e0d89635a9a916cd51d7b41b2d0
SantyMG03 <santymorenog2003@gmail.com>
2026-08-03 18:06:48 +0200

    Created user repository

---
+++ src/main/java/com/santy/finances/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.santy.finances.repositories;
+
+import com.santy.finances.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String name);
+    Optional<User> findByEmail(String email)
+}
