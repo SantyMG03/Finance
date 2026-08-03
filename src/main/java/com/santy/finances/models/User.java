f0243408bb169611888f15eae4dac370eb523fb8
SantyMG03 <santymorenog2003@gmail.com>
2026-08-03 18:02:00 +0200

    Implemented User entity

---
+++ src/main/java/com/santy/finances/models/User.java
@@ -0,0 +1,40 @@
+package com.santy.finances.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Rol para el futuro (ej. ADMIN o USER)
+    @Column(nullable = false)
+    private String role = "USER";
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
